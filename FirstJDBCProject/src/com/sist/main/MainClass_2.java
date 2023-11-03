32d8452c7f5ae1fb5c2b11f078dd9952f92fb18d
DevKim <ksssk96@gmail.com>
2023-11-03 11:20:41 +0900

    first-oracle

---
+++ FirstJDBCProject/src/com/sist/main/MainClass_2.java
@@ -0,0 +1,27 @@
+package com.sist.main;
+/*
+ * 1) Web 개발(사이트)
+ *    
+ *    출력 위치 => 브라우저
+ *              ------- 오라클 연동하는 프로그램이 없다
+ *              HTML / CSS / JavaScript
+ *              => 외의 모든 언어는 브라우저 일반 텍스트
+ *    
+ *    브라우저 ===== 자바  ===== 오라클
+ *           전송        읽기
+ *                     JDBC => Java Database Connective
+ *                     java.sql
+ *                     => 오라클 연결하는 프로그램은
+ *                        자바에 존재하지 않는다
+ *                        => 외부 라이브러리 => ojdbc8.jar
+ *                     파일 :  => .xml , .json , .csv
+ *    실무 (X) => 입문
+ */
+public class MainClass_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
